5fa20d9608295a1c80f7034a889e6eab11ee34f0
Seokmin <ghdtjrald240@gmail.com>
2025-04-29 01:30:57 +0900

    Remove test

--- Formula/fenv.rb
+++ Formula/fenv.rb
@@ -33,6 +33,6 @@ class Fenv < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    # system "false"
   end
 end
