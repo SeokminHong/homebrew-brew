c51ca2fbda960d4a845dace8b12453b6f2c8faf6
Seokmin Hong <ghdtjrald240@gmail.com>
2025-04-28 17:32:40 +0000

    fenv: add 0.1.0 bottle.

--- Formula/fenv.rb
+++ Formula/fenv.rb
@@ -7,6 +7,13 @@ class Fenv < Formula
 
   head "https://github.com/SeokminHong/fenv.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/SeokminHong/homebrew-brew/releases/download/fenv-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe7e7355507a685588daf8464b2ef844f9edc134e2553d9390699314f05ba7ad"
+    sha256 cellar: :any_skip_relocation, ventura:       "dfa6005d732d1149ff46d2c5053880a62064b5470dd9a6d146ddf3f05667a5b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19ecc842e2dd18d70d8911e2424489524c6027bd3fe2e2a206d13c7ea2fe6277"
+  end
+
   depends_on "fish"
 
   def install
