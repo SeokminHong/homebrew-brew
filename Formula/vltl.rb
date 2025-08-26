8f4a9968154a5e4e88a59ae26ec7f384f52fb143
Seokmin Hong <ghdtjrald240@gmail.com>
2025-08-26 03:37:55 +0000

    vltl: add 0.1.3 bottle.

--- Formula/vltl.rb
+++ Formula/vltl.rb
@@ -7,6 +7,13 @@ class Vltl < Formula
 
   head "https://github.com/SeokminHong/vltl.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/SeokminHong/homebrew-brew/releases/download/vltl-0.1.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65063088a03fdbbdea07fb2785c27339613a1bddf082f7f0597fb0b0981d147d"
+    sha256 cellar: :any_skip_relocation, ventura:       "af73867a4811655a99194e087b12abe1a120fb35b9c9f1b13ef682e17901f8cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cfdcc9f68fe8855c79e55c74387b7a46846d23c4edc47c13bc5470399166b242"
+  end
+
   depends_on "rust" => :build
   depends_on "fish"
 
