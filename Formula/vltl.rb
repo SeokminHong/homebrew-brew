17acf93d9c94e196bbdfadf93b2b96f24f29e354
Seokmin Hong <ghdtjrald240@gmail.com>
2025-08-26 12:01:22 +0900

    Remove on_macos and update version

--- Formula/vltl.rb
+++ Formula/vltl.rb
@@ -1,22 +1,20 @@
 class Vltl < Formula
   desc "Fix a 2-set Korean typo to English"
   homepage "https://github.com/SeokminHong/vltl"
-  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "941adbdfdf9542e3f7ea44c4f4be96f04a79916dc087e871bb6df4ea421ecd23"
+  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "1d481cf823a03b718c35b7a2dfc93bfd6d99d68ec747c454eb0981730094c31a"
   license "MIT"
 
   head "https://github.com/SeokminHong/vltl.git", branch: "main"
 
-  on_macos do
-    depends_on "rust" => :build
-    depends_on "fish"
+  depends_on "rust" => :build
+  depends_on "fish"
 
-    def install
-      system "cargo", "install", "vltl", *std_cargo_args
-    end
+  def install
+    system "cargo", "install", "vltl", *std_cargo_args
+  end
 
-    test do
-      assert_match "vltl 0.1.1", shell_output("#{bin}/vltl --version")
-    end
+  test do
+    assert_match "vltl 0.1.2", shell_output("#{bin}/vltl --version")
   end
 end
