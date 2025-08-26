8ab055eac2528293482ae054cb25d1b63cff504a
Seokmin Hong <ghdtjrald240@gmail.com>
2025-08-26 11:25:33 +0900

    Add vltl

---
+++ Formula/vltl.rb
@@ -0,0 +1,20 @@
+class Fenv < Formula
+  desc "Fix a 2-set Korean typo to English"
+  homepage "https://github.com/SeokminHong/vltl"
+  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "941adbdfdf9542e3f7ea44c4f4be96f04a79916dc087e871bb6df4ea421ecd23"
+  license "MIT"
+
+  head "https://github.com/SeokminHong/vltl.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "fish"
+
+  def install
+    system "cargo", "install", "vltl", *std_cargo_args
+  end
+
+  test do
+    assert_match "vltl 0.1.1", shell_output("#{bin}/vltl --version")
+  end
+end
