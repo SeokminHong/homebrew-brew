5cdc4fa2f4c62236e1069cb1f8bb679620c7927c
Seokmin Hong <ghdtjrald240@gmail.com>
2026-03-12 14:11:38 +0900

    Update vltl to 0.1.5

--- Formula/vltl.rb
+++ Formula/vltl.rb
@@ -1,14 +1,14 @@
 class Vltl < Formula
   desc "Fix a 2-set Korean typo to English"
   homepage "https://github.com/SeokminHong/vltl"
-  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "e249e1d324aa7d3f2550f26bc47d4685c5b525597602ccf9b59792b8be4326f6"
+  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "9ec1f8db85a8242b43fdfb0c139608741ea5d6ae2dfecbd978e0ce2981018a33"
   license "MIT"
 
   head "https://github.com/SeokminHong/vltl.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/SeokminHong/homebrew-brew/releases/download/vltl-0.1.4"
+    root_url "https://github.com/SeokminHong/homebrew-brew/releases/download/vltl-0.1.5"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec066e6c68e8430a9d2e9210e2777984f9c514424a26d6ccb995211e65fea4d1"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b5d5e64519b59c0ca273d36d3335424c31bfb472c9d9bbe505f114234ac49f4"
   end
@@ -21,6 +21,6 @@ class Vltl < Formula
   end
 
   test do
-    assert_match "vltl 0.1.4", shell_output("#{bin}/vltl --version")
+    assert_match "vltl 0.1.5", shell_output("#{bin}/vltl --version")
   end
 end
