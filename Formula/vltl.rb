094e32729de487b68c6f72d9ef84682ee6612c0e
Seokmin Hong <ghdtjrald240@gmail.com>
2026-03-25 07:13:58 +0000

    Update vltl to 0.2.1
    Closes #11.
    
    Signed-off-by: Seokmin Hong <ghdtjrald240@gmail.com>

--- Formula/vltl.rb
+++ Formula/vltl.rb
@@ -1,18 +1,11 @@
 class Vltl < Formula
   desc "Fix a 2-set Korean typo to English"
   homepage "https://github.com/SeokminHong/vltl"
-  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "94356abbca38e97415b397d1ab8647b97370142c0a40cf11af316980ce9d045f"
+  url "https://github.com/SeokminHong/vltl/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "e3d5121a4ccaedde56d414f8e0a257489a550ca04718daac432ec3abfdda5350"
   license "MIT"
 
   head "https://github.com/SeokminHong/vltl.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/SeokminHong/homebrew-brew/releases/download/vltl-0.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "370b0ace2c01a4a680bff6599120eb9ab98eba744ee191baebd9a116adc09a7b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6c60ece119510d09599c4529a842dea612e88f218f99a55f1abb9fd5199df209"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ff6a130e91e152627a49ad96d359427598e45aecaad8e35bd08482e5771cc715"
-  end
 
   depends_on "rust" => :build
   depends_on "fish"
@@ -22,6 +15,6 @@ class Vltl < Formula
   end
 
   test do
-    assert_match "vltl 0.2.0", shell_output("#{bin}/vltl --version")
+    assert_match "vltl 0.2.1", shell_output("#{bin}/vltl --version")
   end
 end
