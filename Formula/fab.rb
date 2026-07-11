e87c1d9d539ac76d86bb244853433a4e88d6186e
Seokmin Hong <ghdtjrald240@gmail.com>
2026-07-11 13:34:18 +0000

    fab: add 0.1.1 bottle.

--- Formula/fab.rb
+++ Formula/fab.rb
@@ -7,6 +7,13 @@ class Fab < Formula
 
   head "https://github.com/SeokminHong/fab.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/SeokminHong/homebrew-brew/releases/download/fab-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "45ad88c0ea8344d61e07345f9e7e259be30c04a3e75f4dc0190bf6433bee1ea6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2ab7e9f59e9ddd61caa3a5abedc8a78a4395c9f661535a737478f06269e2b3ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5d384d1e082b701e3b634d6e6f8ca9f88a0b79b0836ed30422ce20cc477ee00"
+  end
+
   depends_on "fish"
 
   def install
